cf5d6106d8882cb78ebb63223b086568419ae601
darker66678 <darker66678@gmail.com>
2021-11-20 15:03:37 +0800

    add some file

--- yolo_format.py
+++ yolo_format.py
@@ -6,6 +6,7 @@ import argparse
 import random
 import pandas as pd
 
+
 def get_parser():
     parser = argparse.ArgumentParser(description='my description')
     parser.add_argument(
@@ -13,6 +14,7 @@ def get_parser():
     parser.add_argument(
         '--test_folder', default='./yolov5/data/test/')
     return parser
+
 
 def get_img_boxes(mat, idx=0):
     bboxs = mat['digitStruct/bbox']
@@ -26,6 +28,7 @@ def get_img_boxes(mat, idx=0):
                 meta[key].append(int(mat[box[key][i][0]][()].item()))
     return meta
 
+
 def convert(size, box):
     dw = size[0]
     dh = size[1]
@@ -38,6 +41,7 @@ def convert(size, box):
     y = y/dh
     h = h/dh
     return (x, y, w, h)
+
 
 if __name__ == '__main__':
     parser = get_parser()
@@ -57,7 +61,7 @@ if __name__ == '__main__':
     for sample_num in val_sample:
         train_imgs = train_imgs[train_imgs[0] != sample_num]
     train_imgs = sorted(list(train_imgs[0].astype(int)))
-    val_sample = sorted(map(int,val_sample))
+    val_sample = sorted(map(int, val_sample))
 
     file = open("./yolov5/data/cfg/train.txt", "w")
     for i in range(len(train_imgs)):
@@ -78,7 +82,7 @@ if __name__ == '__main__':
     for i in range(len(test_imgs)):
         file.write("."+args.test_folder+str(test_imgs[i])+'.png\n')
     file.close()
-    
+
     # get the data in mat file
     mat = h5py.File(
         args.train_folder+'digitStruct.mat', 'r')
@@ -89,14 +93,14 @@ if __name__ == '__main__':
         for key in ['label', 'left', 'top', 'width', 'height']:
             values = get_img_boxes(mat, index)[key]
             attrs[key] = values
-        #file.write(str(len(attrs['label'])))
+        # file.write(str(len(attrs['label'])))
         for j in range(len(attrs['label'])):
-            if attrs['label'][j]==10:
-                attrs['label'][j]=0
+            if attrs['label'][j] == 10:
+                attrs['label'][j] = 0
             file.write(str(int(attrs['left'][j])) + " " + str(int(attrs['top'][j])) + " " + str(int(
                 attrs['width'][j])) + " " + str(int(attrs['height'][j])) + " " + str(int(attrs['label'][j])) + " ")
             file.write("\n")
-        if index % 1000 == 0 and index!=0:
+        if index % 1000 == 0 and index != 0:
             print(index)
     file.close()
 
@@ -107,15 +111,16 @@ if __name__ == '__main__':
     for (dirpath, dirnames, filenames) in walk(mypath):
         txt_name_list.extend(filenames)
         break
-    txt_name_list = sorted(txt_name_list,key=lambda x: int(os.path.splitext(x)[0]))
+    txt_name_list = sorted(
+        txt_name_list, key=lambda x: int(os.path.splitext(x)[0]))
     for txt_name in txt_name_list:
         txt_path = mypath + txt_name
         txt_file = open(txt_path, "r")
-        lines = txt_file.read().split('\n')   
+        lines = txt_file.read().split('\n')
         txt_outpath = outpath + txt_name
         txt_outfile = open(txt_outpath, "w")
-        
-        #Convert the data to YOLO format
+
+        # Convert the data to YOLO format
         ct = 0
         for line in lines:
             if(len(line) >= 2):
@@ -127,12 +132,12 @@ if __name__ == '__main__':
                 ymax = elems[3]
                 cls = elems[4]
                 path = args.train_folder
-                img_path = str(path+'%s.png'%(os.path.splitext(txt_name)[0]))
-                im=Image.open(img_path)
-                w= int(im.size[0])
-                h= int(im.size[1])   
+                img_path = str(path+'%s.png' % (os.path.splitext(txt_name)[0]))
+                im = Image.open(img_path)
+                w = int(im.size[0])
+                h = int(im.size[1])
                 b = (float(xmin), float(xmax), float(ymin), float(ymax))
-                bb = convert((w,h), b)
-                txt_outfile.write(str(cls) + " " + " ".join([str(a) for a in bb]) + '\n')  
-    print("finish!")      
-
+                bb = convert((w, h), b)
+                txt_outfile.write(
+                    str(cls) + " " + " ".join([str(a) for a in bb]) + '\n')
+    print("finish!")
